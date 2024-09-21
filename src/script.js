7d940687201f625d71867378ef1c78dd9979055c
Dorine <anyangodorine474@gmail.com>
2024-09-21 20:43:42 +0300

    modified the files

--- src/script.js
+++ src/script.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+
+    getForecast(response.data.city);
 }
 
 function formateDate(date){
@@ -43,6 +45,45 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector('#search-form-input');
   searchCity(searchInput.value);
+}
+
+function formatDate(timestamp){
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    
+    return days[date.getDay()];
+}
+
+function getForecast(city){
+    let apiKey = '180afbft0d9bbd63o1e48164442b604a';
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+
+    let forecastHtml = "";
+
+
+    response.data.daily.forEach(function(day, index) {
+        if (index < 5) {
+        forecastHtml = forecastHtml + `
+        <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDate(day.time)}</div>
+                <img src = "${day.condition.icon_url}" class="weather-forecast-icon"/>
+                <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                <strong>${Math.round(day.temperature.maximum)}°C</strong>
+                </div>
+                <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°C
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    });
+    let forecastElement = document.querySelector('#forecast');
+    forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector('#search-form');
